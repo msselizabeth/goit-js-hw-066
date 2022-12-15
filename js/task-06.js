0aa1d6567c44244711b5547c728a3ff6f54fab5c
msselizabeth <lizagotovitryby@gmail.com>
2022-12-15 13:56:32 +0200

    fix task

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,8 @@ const inputEl = document.querySelector('#validation-input');
 const onInput = (event) => {    
    let userName = inputEl.value;
     return userName.length === Number(inputEl.dataset.length) 
-    ? inputEl.classList.add('valid') 
-    : inputEl.classList.add('invalid');
+    ? inputEl.classList = 'valid'
+    : inputEl.classList = 'invalid';
     
 }
 
